4191f5d694e56f67cab325816a3d1c066152c068
Łukasz Paszko <lpaszko30@gmail.com>
2020-06-20 12:37:38 +0200

    implementacja klasy ByleJakaKlasa

--- src/main/java/Gui/ByleJakaKlasa.java
+++ src/main/java/Gui/ByleJakaKlasa.java
@@ -1,5 +1,32 @@
 package Gui;
 
-public class ByleJakaKlasa {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
+public class ByleJakaKlasa {
+	private static List<String> lista;
+	static{
+		lista = new ArrayList<String>();
+	}
+	
+	public static void makeList() throws FileNotFoundException {
+		
+		File inputFile = new File("src/main/Java/Gui/input.txt");
+		Scanner inputFileReader = new Scanner(inputFile);
+		
+		while (inputFileReader.hasNextLine()) {
+			lista.add(inputFileReader.nextLine());
+		}
+		
+		inputFileReader.close();
+	}
+	
+	public static void printList() {
+		for(String owoc: lista)
+			System.out.println(owoc);
+	}
+	
 }
--- src/main/java/lkm/Main.java
+++ src/main/java/lkm/Main.java
@@ -1,12 +1,12 @@
 package lkm;
 
+import java.io.FileNotFoundException;
+
+import Gui.ByleJakaKlasa;
+
 public class Main {
-	public static void main(String[] args) {
-		
-		System.out.println("Owoce");
-		/**
-		 * push
-		 */
-		
+	public static void main(String[] args) throws FileNotFoundException {
+		ByleJakaKlasa.makeList();
+		ByleJakaKlasa.printList();
 	}
 }
